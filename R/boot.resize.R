8ac1488d02de5a0fa6be9a2884b97f2642f6f1ab
Clare Clingain <clc586@nyu.edu>
2018-12-09 11:16:55 -0500

    finished bulk of resizing/sampling function, need to fix bugs for 3 levels with resampling at level 2

---
+++ R/boot.resize.R
@@ -0,0 +1,93 @@
+resize.boot <- function(id, group1, group2 = FALSE, data, level, increase = 2){
+
+  boot.samp <- NULL
+
+# If data has only 2 levels
+if(group2 == F){
+  # Find the number of unique groups
+  num.groups <- unique(data[,group1])
+  # If the increase will be at the first level
+  if(level == id){
+    group1 <- data[,group1]
+
+    for(i in num.groups){
+      in.group <- data[group1 == i,]
+      group.samp <- in.group[sample(1:nrow(in.group), nrow(in.group)*increase, replace = T),]
+      boot.samp <- rbind.data.frame(boot.samp, group.samp)
+    }
+    return(boot.samp)
+  }
+  # If the increase will be at the second level
+  else if(level == group1){
+    new <- sample(num.groups, length(num.groups)*increase, replace = T)
+    group1col <- data[,group1]
+    all <- c(num.groups, new)
+    newid <- 1
+
+    for(i in all){
+      in.group <- data[group1col == i,]
+      group.samp <- in.group[sample(1:nrow(in.group), nrow(in.group), replace = T),]
+
+      group.samp$level2id <- newid
+      boot.samp <- rbind.data.frame(boot.samp, group.samp)
+      newid <- newid+1
+    }
+  return(boot.samp)
+  }
+
+}
+# If data has 3 levels
+else if(group2 != FALSE){
+  # If the increase will be at the first level
+  if(level == id){
+    # Find the unique groups for level 3
+    num.groups2 <- unique(data[,group2])
+    group2col <- data[,group2]
+    # Resample within each group
+    for(i in num.groups2){
+      in.group2 <- data[group2col==i,]
+
+      num.groups1 <- unique(in.group2[,group1])
+      for(j in num.groups1){
+        # Get observations that are in current group
+        in.group1 <- in.group2[group1==j,]
+        # sample from within that group
+        group.samp <- in.group2[sample(1:nrow(in.group2), nrow(in.group2)*increase, replace = T),]
+        # Save into a data frame
+        boot.samp <- rbind.data.frame(boot.samp,group.samp)
+       }
+      }
+return(boot.samp)
+    }
+  # If the increase will be at the second level
+  else if(level == group1){
+    # Find the unique groups for level 3
+    num.groups2 <- unique(data[,group2])
+    group2col <- data[,group2]
+    # Resample within each group
+    for(i in num.groups2){
+      # Save a new data set for each specific level
+      in.group2 <- data[group2col==i,]
+      # And find the unique level 2 IDs within each level 3
+      #group1 <- in.group2[,group1]
+      num.groups1 <- unique(in.group2[,group1])
+      # Sample n new level 2 IDs
+      new <- sample(num.groups1, length(num.groups1)*increase, replace = T)
+      group1 <- in.group2[,group1]
+      all <- c(num.groups1, new)
+      newid <- 1
+
+      for(j in all){
+        in.group <- in.group2[group1 == j,]
+        group.samp <- in.group[sample(1:nrow(in.group), nrow(in.group), replace = T),]
+
+        group.samp$level2id <- newid
+        boot.samp <- rbind.data.frame(boot.samp, group.samp)
+        newid <- newid+1
+      }
+    }
+
+   }
+}
+  return(boot.samp)
+}
--- R/power.resize.R
+++ R/power.resize.R
@@ -27,6 +27,77 @@
 power.resize <- function(model, n, id, group1, group2 = FALSE, data, level, increase = 50){
 
 
+  if(group2 == F){ # if we only have 2 levels
+    # Find the unique groups
+    num.groups <- unique(data[,group1])
+    # Initiate bootstrap sample storage
+    boot.samp <- NULL
+
+    # If level to increase is Level 1 (Student/ID)
+    if(level == id){
+      # Get column call ready since can't pass as before
+      group1 <- data[,group1]
+      # Initialize storage
+      #boot.samp <- matrix(ncol = ncol(data), nrow = nrow(data)*(1+(increase/100)))
+      # Increase students within each LEVEL 2 Group (i.e, schools)
+      for(i in num.groups){
+       # Get observations that are in current group
+       in.group <- data[group1==i,]
+       # sample from within that group
+       group.samp <- in.group[sample(1:nrow(in.group), nrow(in.group)*(1+(increase/100)), replace = T),]
+       # Save into a data frame
+       boot.samp <- rbind.data.frame(boot.samp,group.samp)
+       }
+       return(as.data.frame(boot.samp))
+    }
+    # If level to increase is Level 2 (School)
+    else if(level == group1){
+      # Initialize storage
+      #boot.samp <- matrix(ncol = ncol(data), nrow = nrow(data)*(1+(increase/100)))
+      # Randomly select x schools to be the new schools
+      new <- sample(num.groups, length(num.groups)*(increase/100), replace = T)
+      # Get column call ready since can't pass as before
+      group1 <- data[,group1]
+      for(i in 1:new){
+        in.group <- data[group1 == i,]
+        group.samp <- in.group[sample(1:nrow(in.group), nrow(in.group), replace = T),]
+        boot.samp <- rbind.data.frame(boot.samp, group.samp)
+      }
+      return(boot.samp)
+    }
+
+
+  }
+
+  else if (group2 != F){ # if we have 3 levels
+    # Find the unique groups for level 3
+    num.groups2 <- unique(data[,group2])
+    # Find the unique groups for level 2
+    #num.groups1 <- unique(data[,group1])
+    # Find the unique IDs
+    num.ids <- unique(data[,id])
+
+    # Initiate bootstrap sample storage
+    boot.samp <- NULL
+    # Get column call ready since can't pass as before
+    #group1 <- data[,group1]
+    group2col <- data[,group2]
+    # Resample within each group
+    for(i in num.groups2){
+      in.group2 <- data[group2col==i,]
+
+      num.groups1 <- unique(in.group2[,group2])
+      for(j in num.groups1){
+        # Get observations that are in current group
+        in.group1 <- in.group2[group1==j,]
+        # sample from within that group
+        group.samp <- in.group2[sample(1:nrow(in.group2), nrow(in.group2), replace = T),]
+        # Save into a data frame
+        boot.samp <- rbind.data.frame(boot.samp,group.samp)
+      }
+    }
+  }
+  return(boot.samp)
 
 
 
